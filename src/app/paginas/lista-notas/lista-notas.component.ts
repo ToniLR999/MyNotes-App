195eab2c45ccb830aad23cca9c6a9ec4861507d5
Toni <tonilupianezroman@gmail.com>
2021-10-28 20:12:14 +0200

    Filtro

--- src/app/paginas/lista-notas/lista-notas.component.ts
+++ src/app/paginas/lista-notas/lista-notas.component.ts
@@ -17,8 +17,39 @@ export class ListaNotasComponent implements OnInit {
     this.notas = this.notasService.getAll();
   }
 
-  deleteNote(id: number){
+  deleteNota(id: number){
     this.notasService.delete(id);
+
+  }
+
+  filtro(query: string){
+    query = query.toLowerCase().trim();
+    
+    let allResults: Nota[] = new Array<Nota>();
+
+    let terms: string[] = query.split(' ');
+
+    terms = this.borrarDuplicadas(terms);
+
+  }
+
+  borrarDuplicadas(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+
+    arr.forEach(e => uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  } 
+
+  relevantNotes(query: string){
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notas.filter(nota => {
+      if(nota.body.toLowerCase().includes(query) || nota.titulo.toLowerCase().includes(query)){
+        return true;
+      }
+      return false;
+    })
+
 
   }
 }
